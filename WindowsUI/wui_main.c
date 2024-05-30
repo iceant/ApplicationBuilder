d291d5d31af599f4302388a5962f021ea98be3b8
Chen Peng <pizer.chen@gmail.com>
2024-05-30 19:28:37 +0800

    动态改变窗体背景

--- WindowsUI/wui_main.c
+++ WindowsUI/wui_main.c
@@ -7,10 +7,20 @@
 static wui_window_t MainFrame;
 char Directions[100];
 
+
 static LRESULT wui_main__on_create(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam){
     LPCREATESTRUCT lpCreateStruct = (LPCREATESTRUCT)(lParam);
     strcpy(Directions, "Try resizing this window");
-    return 1;
+    HBRUSH brush = CreateSolidBrush(RGB(0, 0, 255));
+    SetClassLongPtr(hwnd, GCLP_HBRBACKGROUND, (LONG_PTR)brush);
+
+
+    return WUI_WINDOW_SUCCESS;
+}
+
+static LRESULT wui_main__on_destroy(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam){
+    PostQuitMessage(0);
+    return WUI_WINDOW_SUCCESS;
 }
 
 static LRESULT wui_main__on_paint(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam){
@@ -26,8 +36,6 @@ static LRESULT wui_main__on_paint(HWND hwnd, UINT message, WPARAM wParam, LPARAM
     GetClientRect(hwnd, &Rect);
 
     DrawText(PaintDC, "The middle of the road", -1, &Rect, DT_SINGLELINE|DT_CENTER|DT_VCENTER);
-
-
 
     EndPaint(hwnd, &PaintStruct);
 
@@ -89,6 +97,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdPa
     }
 
     wui_window_register_message_handler(&MainFrame, WM_PAINT, wui_main__on_paint, 0);
+    wui_window_register_message_handler(&MainFrame, WM_DESTROY, wui_main__on_destroy, 0);
     wui_window_register_message_handler(&MainFrame, WM_SHOWWINDOW, wui_main__on_create, 0);
 
     wui_window_create(&MainFrame);
